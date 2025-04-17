80bbfbc0794e348b3352efc233baae409954f0d7
TheGeekMax <maximesanciaume@gmail.com>
2025-04-17 08:37:11 +0200

    added css and started form

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,72 @@
 <template>
-    <p>nb de semaine entre l'année de naissance et votre naissance {{ firstYearWeekCount }}</p>
-    <p>nb d'année complete entre votre naissance et aujourd'hui {{ fullYearCount }}</p>
-    <p>nb de semaine passé depuis le debut de cette année {{ lastYearWeekCount }}</p>
-    <div id="livegrid">
+    <!-- FORM 1: Birth date -->
+    <section class="section" id="form1" >
+        <form @submit.prevent="fromf1tof2">
+            <div class="birth-fields">
+                <input type="number" value="10" placeholder="Day" min="1" max="31" required />
+                <input type="number" value="10" placeholder="Month" min="1" max="12" required />
+                <input type="number" value="2000" placeholder="Year" min="1900" max="2025" required />
+            </div>
+            <button type="submit">Next</button>
+        </form>
+    </section>
 
-    </div>
+    <!-- FORM 2: Gender -->
+    <section class="section" id="form2">
+        <form>
+            <div class="gender-options">
+                <label>
+                <input type="radio" name="gender" value="male"/>
+                <span class="gender-label">Male</span>
+                </label>
+                <label>
+                <input type="radio" name="gender" value="female" />
+                <span class="gender-label">Female</span>
+                </label>
+                <label>
+                <input type="radio" name="gender" value="other" checked />
+                <span class="gender-label">Other / Prefer not to say</span>
+                </label>
+            </div>
+            <button type="submit">Next</button>
+        </form>
+    </section>
+
+    <!-- FORM 3: Region -->
+    <section class="section" id="form3">
+        <form>
+            <select required>
+                <option value="europe" selected>Europe</option>
+                <option value="north-america">North America</option>
+            </select>
+            <button type="submit">Show Calendar</button>
+        </form>
+    </section>
+
+    <!-- Memento Mori -->
+    <section id="memento-mori">
+        <div id="grid-alive" class="grid"></div>
+    </section>
 </template>
 
 <script setup>
 
 //user defined values
-let year = 2003
-let month = 3
-let day = 9
+let year = 2024
+let month = 0
+let day = 10
 
 let livespan = 85;
+
+//forms
+function fromf1tof2(){
+    console.log("form1 to form2");
+    //handle values
+
+    //set anchor to form2 
+    let form1 = document.getElementById("form1");
+    form1.classList.add("hidden");
+}
 
 //calculated values
 let firstYearWeekCount = ref(0);
@@ -56,73 +108,67 @@ function isYearLeap(year) {
 function generateGridFromValue(value){
     const GENERATE_NONEBOX = function (){
         let noneBox = document.createElement("div");
-        noneBox.className = "grid-box none";
-        noneBox.style.backgroundColor = "white";
-        noneBox.innerHTML="."
+        noneBox.className = "week invisible";
         return noneBox;
     }
 
     const GENERATE_PASSEDBOX = function (){
         let passedBox = document.createElement("div");
-        passedBox.className = "grid-box passed";
-        passedBox.style.backgroundColor = "green";
-        passedBox.innerHTML="."
+        passedBox.className = "week past";
         return passedBox;
     }
 
     const GENERATE_FUTUREBOX = function (){
         let futureBox = document.createElement("div");
-        futureBox.className = "grid-box future";
-        futureBox.style.backgroundColor = "red";
-        futureBox.innerHTML="."
+        futureBox.className = "week future";
         return futureBox;
     }
 
-    let grid = document.getElementById("livegrid");
+    let grid = document.getElementById("grid-alive");
 
     //add first year column
     let firstYear = document.createElement("div");
-    firstYear.className = "grid-column";
+    firstYear.className = "year-column";
     let i = 0
     for(; i < firstYearWeekCount.value; i++){
-        firstYear.appendChild(GENERATE_NONEBOX());
+        firstYear.prepend(GENERATE_NONEBOX());
     }
     let maxWeek = isYearLeap(year) ? 53 : 52;
     for(; i < maxWeek; i++){
-        firstYear.appendChild(GENERATE_PASSEDBOX());
+        firstYear.prepend(GENERATE_PASSEDBOX());
     }
     if(maxWeek == 52){
-        firstYear.appendChild(GENERATE_NONEBOX());
+        firstYear.prepend(GENERATE_NONEBOX());
     }
     grid.appendChild(firstYear);
 
     //add full year columns
     for(let i = 0; i < fullYearCount.value; i++){
         let yearColumn = document.createElement("div");
-        yearColumn.className = "grid-column";
+        yearColumn.className = "year-column";
         let maxWeek = isYearLeap(year + i + 1) ? 53 : 52;
         for(let j = 0; j < maxWeek; j++){
-            yearColumn.appendChild(GENERATE_PASSEDBOX());
+            yearColumn.prepend(GENERATE_PASSEDBOX());
         }
         if(maxWeek == 52){
-            yearColumn.appendChild(GENERATE_NONEBOX());
+            yearColumn.prepend(GENERATE_NONEBOX());
         }
         grid.appendChild(yearColumn);
     }
 
     //add last year column
     let lastYear = document.createElement("div");
-    lastYear.className = "grid-column";
+    lastYear.className = "year-column";
     i = 0;
     for(; i < lastYearWeekCount.value; i++){
-        lastYear.appendChild(GENERATE_PASSEDBOX());
+        lastYear.prepend(GENERATE_PASSEDBOX());
     }
     maxWeek = isYearLeap(year + fullYearCount.value + 1) ? 53 : 52;
     for(; i < maxWeek; i++){
-        lastYear.appendChild(GENERATE_FUTUREBOX());
+        lastYear.prepend(GENERATE_FUTUREBOX());
     }
     if(maxWeek == 52){
-        lastYear.appendChild(GENERATE_NONEBOX());
+        lastYear.prepend(GENERATE_NONEBOX());
     }
     grid.appendChild(lastYear);
 
@@ -130,13 +176,13 @@ function generateGridFromValue(value){
     let curYearpassed = fullYearCount.value + 1;
     for(let i = curYearpassed; i < livespan; i++){
         let yearColumn = document.createElement("div");
-        yearColumn.className = "grid-column";
+        yearColumn.className = "year-column";
         let maxWeek = isYearLeap(year + i) ? 53 : 52;
         for(let j = 0; j < maxWeek; j++){
-            yearColumn.appendChild(GENERATE_FUTUREBOX());
+            yearColumn.prepend(GENERATE_FUTUREBOX());
         }
         if(maxWeek == 52){
-            yearColumn.appendChild(GENERATE_NONEBOX());
+            yearColumn.prepend(GENERATE_NONEBOX());
         }
         grid.appendChild(yearColumn);
     }
@@ -152,53 +198,11 @@ onMounted(() => {
 
 </script>
 
-<style lang="css" scoped>
-:root {
-  --cell-size: 15px; /* Change this value to adjust cell size */
-  --cell-gap: 1px;
-  --column-gap: 2px;
-}
+<style lang="css">
+    @import url("~/assets/css/main.css");
 
-#livegrid {
-  display: flex;
-  flex-direction: column;
-  gap: var(--column-gap);
-  overflow-x: auto;
-  padding: 20px 0;
-  max-width: 100%;
-  background-color: #f5f5f5;
-  border-radius: 8px;
-}
+    @import url("~/assets/css/memento_pc.css");
+    @import url("~/assets/css/forms_pc.css");
 
-.grid-column {
-  display: flex;
-  flex-direction: column;
-  gap: var(--cell-gap);
-}
-
-.grid-box {
-  width: var(--cell-size);
-  height: var(--cell-size);
-  border-radius: 2px;
-  box-sizing: border-box;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-}
-
-.grid-box:hover {
-  transform: scale(1.2);
-  z-index: 1;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-}
-
-.grid-box.passed {
-  border-color: rgba(0, 128, 0, 0.3);
-}
-
-.grid-box.future {
-  border-color: rgba(255, 0, 0, 0.3);
-}
-
-.grid-box.none {
-  border: 1px dashed #ccc;
-}
+    
 </style>
